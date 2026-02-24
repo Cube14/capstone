0cc983da11b699174c3980db4155b0ccfc9d47f1
Cube14 <shivansh2005@gmail.com>
2026-02-24 23:32:54 +0530

    error for jenkins

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
-// API base URL for Docker + local development
-
 export const API_URL =
-  process.env.REACT_APP_API_URL || "http://backend:5000";
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
